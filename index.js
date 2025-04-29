82bbc096eb1f182e5dc516dabb15c362df758dfd
Saqib Ahmad <saqibahmad0404@gmail.com>
2025-04-29 15:14:59 +0600

    post command handled

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000;
 
-// espresso
-// mp0TEoaYdiClHcgH
+app.use(express.json());
+app.use(cors());
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://espresso:mp0TEoaYdiClHcgH@cluster0.tlbypdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.tlbypdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -17,23 +20,37 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
+        const coffees = client.db("espresso").collection("coffees")
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+        })
+
+        app.post('/', async (req, res) => {
+            const coffee = req.body;
+            const result = await coffees.insertOne(coffee)
+
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
